7d42ff28572ddbe805c62c2ba57850d6b2c93269
Wiiliam Pabor <wiiliampabor@VeritasPays-MacBook-Pro-3.local>
2024-05-31 23:36:47 +0800

    Clean slate

--- app/index.tsx
+++ app/index.tsx
@@ -1,12 +1,62 @@
-import { Link, router } from "expo-router";
-import { View, Text, Pressable } from "react-native";
+import { Link, router } from 'expo-router';
+import { View, Text, Pressable, StyleSheet, TouchableOpacity } from 'react-native';
+import { useSampleStore } from '@/store/sampleStore';
 
 const HomePage = () => {
+  const count = useSampleStore((state) => state.count);
+  const increment = useSampleStore((state) => state.increment);
+  const decrement = useSampleStore((state) => state.decrement);
+  const reset = useSampleStore((state) => state.reset);
   return (
-    <View>
-      <Text>Hello</Text>
+    <View style={styles.container}>
+      <Text style={[styles.textCenter, { marginVertical: 10, marginHorizontal: 50 }]}>
+        Hello, This is a boilerplate with Zustand as State Management and Expo Router
+      </Text>
+
+      <Text style={styles.textCenter}>Test the zustand count state: {count}</Text>
+
+      <View style={styles.buttonView}>
+        <TouchableOpacity
+          onPress={decrement}
+          style={[styles.buttonStyle, { backgroundColor: 'red' }]}
+        >
+          <Text style={{ color: 'white' }}>Decrement</Text>
+        </TouchableOpacity>
+        <TouchableOpacity
+          onPress={increment}
+          style={[styles.buttonStyle, { backgroundColor: 'blue' }]}
+        >
+          <Text style={{ color: 'white' }}>Increment</Text>
+        </TouchableOpacity>
+        <TouchableOpacity
+          onPress={reset}
+          style={[styles.buttonStyle, { backgroundColor: 'yellow' }]}
+        >
+          <Text>Reset</Text>
+        </TouchableOpacity>
+      </View>
     </View>
   );
 };
 
 export default HomePage;
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  textCenter: {
+    textAlign: 'center',
+  },
+  buttonView: {
+    flexDirection: 'row',
+    justifyContent: 'space-around',
+  },
+  buttonStyle: {
+    padding: 10,
+    margin: 10,
+    borderRadius: 10,
+  },
+});
